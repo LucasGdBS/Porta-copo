3501da16ddbe2400bde37df0d0761463b8efe749
LucasGdbs <lgbs@cesar.school>
2025-04-07 18:59:56 -0300

    feat: adding potenciometer

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -45,6 +45,8 @@
 /* USER CODE END PM */
 
 /* Private variables ---------------------------------------------------------*/
+ADC_HandleTypeDef hadc1;
+
 I2C_HandleTypeDef hi2c1;
 
 UART_HandleTypeDef huart2;
@@ -62,10 +64,12 @@ void SystemClock_Config(void);
 static void MX_GPIO_Init(void);
 static void MX_USART2_UART_Init(void);
 static void MX_I2C1_Init(void);
+static void MX_ADC1_Init(void);
 void StartDefaultTask(void const * argument);
 
 /* USER CODE BEGIN PFP */
 void lcd_write();
+void readVoltage();
 void StartWeightTask(void *argument);
 
 /* USER CODE END PFP */
@@ -106,6 +110,7 @@ int main(void)
   MX_GPIO_Init();
   MX_USART2_UART_Init();
   MX_I2C1_Init();
+  MX_ADC1_Init();
   /* USER CODE BEGIN 2 */
   HD44780_Init(2);
   HD44780_Clear();
@@ -141,7 +146,9 @@ int main(void)
   /* USER CODE BEGIN RTOS_THREADS */
   /* add threads, ... */
   xTaskCreate(lcd_write, "lcd_write", configMINIMAL_STACK_SIZE, NULL, tskIDLE_PRIORITY, NULL);
-  xTaskCreate(StartWeightTask, "WeightTask", configMINIMAL_STACK_SIZE, NULL, tskIDLE_PRIORITY, NULL);
+  xTaskCreate(readVoltage, "readVoltage", configMINIMAL_STACK_SIZE, NULL, tskIDLE_PRIORITY, NULL);
+
+  // xTaskCreate(StartWeightTask, "WeightTask", configMINIMAL_STACK_SIZE, NULL, tskIDLE_PRIORITY, NULL);
   /* USER CODE END RTOS_THREADS */
 
   /* Start scheduler */
@@ -207,6 +214,73 @@ void SystemClock_Config(void)
   {
     Error_Handler();
   }
+}
+
+/**
+  * @brief ADC1 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_ADC1_Init(void)
+{
+
+  /* USER CODE BEGIN ADC1_Init 0 */
+
+  /* USER CODE END ADC1_Init 0 */
+
+  ADC_MultiModeTypeDef multimode = {0};
+  ADC_ChannelConfTypeDef sConfig = {0};
+
+  /* USER CODE BEGIN ADC1_Init 1 */
+
+  /* USER CODE END ADC1_Init 1 */
+
+  /** Common config
+  */
+  hadc1.Instance = ADC1;
+  hadc1.Init.ClockPrescaler = ADC_CLOCK_ASYNC_DIV1;
+  hadc1.Init.Resolution = ADC_RESOLUTION_12B;
+  hadc1.Init.DataAlign = ADC_DATAALIGN_RIGHT;
+  hadc1.Init.ScanConvMode = ADC_SCAN_DISABLE;
+  hadc1.Init.EOCSelection = ADC_EOC_SINGLE_CONV;
+  hadc1.Init.LowPowerAutoWait = DISABLE;
+  hadc1.Init.ContinuousConvMode = DISABLE;
+  hadc1.Init.NbrOfConversion = 1;
+  hadc1.Init.DiscontinuousConvMode = DISABLE;
+  hadc1.Init.ExternalTrigConv = ADC_SOFTWARE_START;
+  hadc1.Init.ExternalTrigConvEdge = ADC_EXTERNALTRIGCONVEDGE_NONE;
+  hadc1.Init.DMAContinuousRequests = DISABLE;
+  hadc1.Init.Overrun = ADC_OVR_DATA_PRESERVED;
+  hadc1.Init.OversamplingMode = DISABLE;
+  if (HAL_ADC_Init(&hadc1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /** Configure the ADC multi-mode
+  */
+  multimode.Mode = ADC_MODE_INDEPENDENT;
+  if (HAL_ADCEx_MultiModeConfigChannel(&hadc1, &multimode) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /** Configure Regular Channel
+  */
+  sConfig.Channel = ADC_CHANNEL_5;
+  sConfig.Rank = ADC_REGULAR_RANK_1;
+  sConfig.SamplingTime = ADC_SAMPLETIME_2CYCLES_5;
+  sConfig.SingleDiff = ADC_SINGLE_ENDED;
+  sConfig.OffsetNumber = ADC_OFFSET_NONE;
+  sConfig.Offset = 0;
+  if (HAL_ADC_ConfigChannel(&hadc1, &sConfig) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN ADC1_Init 2 */
+
+  /* USER CODE END ADC1_Init 2 */
+
 }
 
 /**
@@ -349,24 +423,58 @@ static void MX_GPIO_Init(void)
 
 /* USER CODE BEGIN 4 */
 void lcd_write(){
-	while (1){
-		while(HAL_UART_Receive(&huart2, &caracter, 1, HAL_MAX_DELAY) != HAL_OK);
+	char buffer[16];
+	char modo = 0; // 0 = nada, 'T' = temperatura, 'P' = peso
 
-		if (caracter == 't' || caracter == 'T'){
-			HD44780_Clear();
-			HD44780_Backlight();
-			HD44780_SetCursor(0,0);
-			HD44780_PrintStr("Temperatura:");
+	while (1){
+		// Verifica se recebeu algo via UART
+		if (HAL_UART_Receive(&huart2, &caracter, 1, 10) == HAL_OK) {
+			if (caracter == 't' || caracter == 'T') {
+				modo = 'T';
+				HD44780_Clear();
+				HD44780_Backlight();
+				HD44780_SetCursor(0,0);
+				HD44780_PrintStr("Temperatura:");
+			} else if (caracter == 'p' || caracter == 'P') {
+				modo = 'P';
+				HD44780_Clear();
+				HD44780_Backlight();
+				HD44780_SetCursor(0,0);
+				HD44780_PrintStr("Peso:");
+			}
+		}
+
+		if (modo == 'T') {
+			int valor = HAL_ADC_GetValue(&hadc1); // valor do potenciômetro
+			snprintf(buffer, sizeof(buffer), "%d C", valor);
 			HD44780_SetCursor(0,1);
-			HD44780_PrintStr("24.2C");
-		} else if (caracter == 'P' || caracter == 'p'){
-			HD44780_Clear();
-			HD44780_Backlight();
-			HD44780_SetCursor(0,0);
-			HD44780_PrintStr("Peso:");
+			HD44780_PrintStr("                "); // Limpa a linha antes de reescrever
+			HD44780_SetCursor(0,1);
+			HD44780_PrintStr(buffer);
+		} else if (modo == 'P') {
+			// Aqui você poderia atualizar peso se quisesse no futuro
 			HD44780_SetCursor(0,1);
 			HD44780_PrintStr("0.30g");
 		}
+
+		HAL_Delay(500); // Atualiza a cada 500ms
+	}
+}
+
+void readVoltage(){
+	while (1){
+		HAL_ADC_Start(&hadc1);
+
+
+		HAL_ADC_PollForConversion(&hadc1, HAL_MAX_DELAY);
+
+		int value = HAL_ADC_GetValue(&hadc1);
+
+		printf("%d\n\r", value);
+
+		osDelay(50);
+
+		// HAL_UART_Transmit(&huart2, (uint32_t*)buffer, strlen(buffer), HAL_MAX_DELAY);
 	}
 }
 
